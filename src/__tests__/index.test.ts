38d1fe7941a6533d7a7379ae4271898227f8396c
AnyaWMa <wanjingma1122@gmail.com>
2022-07-27 12:07:52 -0700

    1.0.1

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,38 +1,33 @@
 import { itemResponseFunction } from '../index';
 import { estimateAbility } from '../index';
-import { findNextItem } from "../index";
-import { Stimulus } from "../index";
+import { findNextItem } from '../index';
+import { Stimulus } from '../index';
 
-describe("itemResponseFunction", () => {
-    it("correctly calculates the probability", () => {
-        expect(0.7234).toBeCloseTo(
-            itemResponseFunction(0, { a: 1, b: -0.3, c: 0.35, d: 1 }),
-            2
-        );
+describe('itemResponseFunction', () => {
+  it('correctly calculates the probability', () => {
+    expect(0.7234).toBeCloseTo(itemResponseFunction(0, { a: 1, b: -0.3, c: 0.35, d: 1 }), 2);
 
-        expect(0.50).toBeCloseTo(
-            itemResponseFunction(0, { a: 1, b: 0, c: 0, d: 1 }),
-            2
-        );
+    expect(0.5).toBeCloseTo(itemResponseFunction(0, { a: 1, b: 0, c: 0, d: 1 }), 2);
 
-        expect(0.625).toBeCloseTo(
-            itemResponseFunction(0, { a: 0.5, b: 0, c: 0.25, d: 1 }),
-            2
-        );
-    });
+    expect(0.625).toBeCloseTo(itemResponseFunction(0, { a: 0.5, b: 0, c: 0.25, d: 1 }), 2);
+  });
 });
 
-describe("estimateAbility", () => {
-    it("correctly estimates the ability", () => {
-        expect(-1.642307).toBeCloseTo(
-            estimateAbility([1, 0, 1],
-                [{a: 2.225, b: -1.885, c: 0.210, d: 1},
-                    {a: 1.174, b: -2.411, c: 0.212, d: 1},
-                    {a: 2.104, b: -2.439, c: 0.192, d: 1}],
-                'MLE'),
-            2
-        );
-        /*
+describe('estimateAbility', () => {
+  it('correctly estimates the ability', () => {
+    expect(-1.642307).toBeCloseTo(
+      estimateAbility(
+        [1, 0, 1],
+        [
+          { a: 2.225, b: -1.885, c: 0.21, d: 1 },
+          { a: 1.174, b: -2.411, c: 0.212, d: 1 },
+          { a: 2.104, b: -2.439, c: 0.192, d: 1 },
+        ],
+        'MLE',
+      ),
+      2,
+    );
+    /*
         expect(0.1635256).toBeCloseTo(
             estimateAbility([1, 1, 1],
                 [{a: 2.225, b: -1.885, c: 0.210, d: 1},
@@ -42,21 +37,17 @@ describe("estimateAbility", () => {
             2
         );
          */
-    });
+  });
 });
 
-const s1 : Stimulus = {difficulty: 0.5, word: 'hello'};
-const s2 : Stimulus = {difficulty: 3, word: 'hi'};
-const s3 : Stimulus = {difficulty: -1.8, word: "greeting"};
+const s1: Stimulus = { difficulty: 0.5, word: 'hello' };
+const s2: Stimulus = { difficulty: 3, word: 'hi' };
+const s3: Stimulus = { difficulty: -1.8, word: 'greeting' };
 
-
-describe("findNextItem", () => {
-    it("correctly suggests the next item", () => {
-        const expected = {nextStimulus: s1,
-            remainingStimuli: [s3, s2]};
-        const received = findNextItem([s1, s2, s3],0,'MFI',true);
-        expect(received).toEqual(expected);
-
-    });
+describe('findNextItem', () => {
+  it('correctly suggests the next item', () => {
+    const expected = { nextStimulus: s1, remainingStimuli: [s3, s2] };
+    const received = findNextItem([s1, s2, s3], 0, 'MFI', true);
+    expect(received).toEqual(expected);
+  });
 });
-
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import {minimize_Powell} from "optimization-js";
-import { cloneDeep } from "lodash";
+import { minimize_Powell } from 'optimization-js';
+import { cloneDeep } from 'lodash';
 
-export type Zeta = { a: number, b: number, c: number, d: number };
+export type Zeta = { a: number; b: number; c: number; d: number };
 
 export interface Stimulus {
-    difficulty: number;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    [key: string]: any;
+  difficulty: number;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any;
 }
 
 /**
@@ -16,8 +16,8 @@ export interface Stimulus {
  * @returns {number} the probability
  */
 export const itemResponseFunction = (theta: number, zeta: Zeta) => {
-    return zeta.c + (zeta.d - zeta.c) / (1 + Math.exp(-zeta.a * (theta - zeta.b)));
-}
+  return zeta.c + (zeta.d - zeta.c) / (1 + Math.exp(-zeta.a * (theta - zeta.b)));
+};
 
 /**
  * return a Gaussian distribution within a given range
@@ -28,20 +28,17 @@ export const itemResponseFunction = (theta: number, zeta: Zeta) => {
  * @param stepSize - the quantization (step size) of the internal table, default = 0.1
  * @returns {Array<[number, number]>} - a normal distribution
  */
-export const normal = (mean=0, stdDev = 1, min = -4, max = 4, stepSize = 0.1) => {
-    const distribution = [];
-    for (let i = min; i <= max; i += stepSize) {
-        distribution.push([i, y(i)]);
-    }
-    return distribution;
+export const normal = (mean = 0, stdDev = 1, min = -4, max = 4, stepSize = 0.1) => {
+  const distribution = [];
+  for (let i = min; i <= max; i += stepSize) {
+    distribution.push([i, y(i)]);
+  }
+  return distribution;
 
-    function y(x: number) {
-        return (
-            (1 / (Math.sqrt(2 * Math.PI) * stdDev)) *
-            Math.exp(-Math.pow(x - mean, 2) / (2 * Math.pow(stdDev, 2)))
-        );
-    }
-}
+  function y(x: number) {
+    return (1 / (Math.sqrt(2 * Math.PI) * stdDev)) * Math.exp(-Math.pow(x - mean, 2) / (2 * Math.pow(stdDev, 2)));
+  }
+};
 
 export const abilityPrior = normal();
 
@@ -55,60 +52,62 @@ export const abilityPrior = normal();
  * @param prior - the prior distribution
  * @returns {number} - the estimate ability based on an estimation method
  */
-export const estimateAbility = (answers:  Array<0 | 1>,
-                                zetas: Array<Zeta>,
-                                method = 'MLE',
-                                minTheta = -4,
-                                maxTheta = 4,
-                                prior = abilityPrior) => {
-    method = method.toLowerCase();
-    const validMethod: Array<string> = ['mle', 'eap']; // TO DO: add staircase
-    if (!validMethod.includes(method)){
-        throw new Error('The method you provided is not in the list of valid methods');
-    }
-    if (method === 'eap'){
-        return estimateAbilityEAP();
-    } else if (method === 'mle') {
-        return estimateAbilityMLE();
-    }
+export const estimateAbility = (
+  answers: Array<0 | 1>,
+  zetas: Array<Zeta>,
+  method = 'MLE',
+  minTheta = -4,
+  maxTheta = 4,
+  prior = abilityPrior,
+) => {
+  method = method.toLowerCase();
+  const validMethod: Array<string> = ['mle', 'eap']; // TO DO: add staircase
+  if (!validMethod.includes(method)) {
+    throw new Error('The method you provided is not in the list of valid methods');
+  }
+  if (method === 'eap') {
+    return estimateAbilityEAP();
+  } else if (method === 'mle') {
+    return estimateAbilityMLE();
+  }
 
-    function estimateAbilityEAP(){
-        let num = 0;
-        let nf = 0;
-        for (let i = 0; i < prior.length; i++) {
-            const theta = prior[i][0];
-            const probability = prior[i][1];
-            const like = likelihood(theta);
-            num += theta * like * probability;
-            nf += like * probability;
-        }
-        return num / nf;
+  function estimateAbilityEAP() {
+    let num = 0;
+    let nf = 0;
+    for (let i = 0; i < prior.length; i++) {
+      const theta = prior[i][0];
+      const probability = prior[i][1];
+      const like = likelihood(theta);
+      num += theta * like * probability;
+      nf += like * probability;
     }
+    return num / nf;
+  }
 
-    function estimateAbilityMLE(){
-        const theta0 = [0];
-        const solution = minimize_Powell(negLikelihood, theta0);
+  function estimateAbilityMLE() {
+    const theta0 = [0];
+    const solution = minimize_Powell(negLikelihood, theta0);
 
-        let theta = solution.argument[0];
-        if (theta > maxTheta) {
-            theta = maxTheta;
-        } else if (theta < minTheta) {
-            theta = minTheta;
-        }
-        return theta;
+    let theta = solution.argument[0];
+    if (theta > maxTheta) {
+      theta = maxTheta;
+    } else if (theta < minTheta) {
+      theta = minTheta;
     }
+    return theta;
+  }
 
-    function negLikelihood(thetaArray: Array<number>) {
-        return -likelihood(thetaArray[0])
-    }
+  function negLikelihood(thetaArray: Array<number>) {
+    return -likelihood(thetaArray[0]);
+  }
 
-    function likelihood(theta: number) {
-        return zetas.reduce((acc, zeta, i) => {
-            const irf = itemResponseFunction(theta, zeta);
-            return answers[i] === 1 ? acc + Math.log(irf) : acc + Math.log(1 - irf);
-        }, 1);
-    }
-}
+  function likelihood(theta: number) {
+    return zetas.reduce((acc, zeta, i) => {
+      const irf = itemResponseFunction(theta, zeta);
+      return answers[i] === 1 ? acc + Math.log(irf) : acc + Math.log(1 - irf);
+    }, 1);
+  }
+};
 
 /**
  * find the next available item from an input array of stimuli based on a selection method
@@ -119,123 +118,119 @@ export const estimateAbility = (answers:  Array<0 | 1>,
  * @returns {nextStimulus: Stimulus,
             remainingStimuli: Array<Stimulus>}
  */
-export const findNextItem = (stimuli: Stimulus[],
-                             theta = 0,
-                             method = 'MFI',
-                             deepCopy = true) => {
-    method = method.toLowerCase();
-    const validMethod: Array<string> = ['mfi', 'random', 'closest'];
-    if (!validMethod.includes(method)){
-        throw new Error('The method you provided is not in the list of valid methods');
-    }
-    let arr: Array<Stimulus>;
-    if (deepCopy) {
-        arr = cloneDeep(stimuli);
+export const findNextItem = (stimuli: Stimulus[], theta = 0, method = 'MFI', deepCopy = true) => {
+  method = method.toLowerCase();
+  const validMethod: Array<string> = ['mfi', 'random', 'closest'];
+  if (!validMethod.includes(method)) {
+    throw new Error('The method you provided is not in the list of valid methods');
+  }
+  let arr: Array<Stimulus>;
+  if (deepCopy) {
+    arr = cloneDeep(stimuli);
+  } else {
+    arr = stimuli;
+  }
+  arr.sort((a, b) => a.difficulty - b.difficulty);
+
+  method = method.toLowerCase();
+  if (method === 'mfi') {
+    const stimuliAddFisher = arr.map((element) => ({
+      fisherInformation: fisherInformation(theta, { a: 1, b: element.difficulty, c: 0.5, d: 1 }),
+      ...element,
+    }));
+    stimuliAddFisher.sort((a, b) => b.fisherInformation - a.fisherInformation);
+    stimuliAddFisher.forEach((stimulus: Stimulus) => {
+      delete stimulus['fisherInformation'];
+    });
+    return {
+      nextStimulus: stimuliAddFisher[0],
+      remainingStimuli: stimuliAddFisher.slice(1),
+    };
+  } else if (method === 'random') {
+    let index: number;
+    if (arr.length < 5) {
+      index = Math.floor(arr.length / 2);
     } else {
-        arr = stimuli;
+      index = Math.floor(arr.length / 2) + randomInteger(-2, 2);
     }
-    arr.sort((a, b) => a.difficulty - b.difficulty);
+    const nextItem = arr[index];
+    arr.splice(index, 1);
+    return {
+      nextStimulus: nextItem,
+      remainingStimuli: arr,
+    };
+  } else if (method === 'closest') {
+    //findClosest requires arr is sorted by difficulty
+    const index = findClosest(arr, theta + 0.481);
+    const nextItem = arr[index];
+    arr.splice(index, 1);
+    return {
+      nextStimulus: nextItem,
+      remainingStimuli: arr,
+    };
+  }
 
-    method = method.toLowerCase();
-    if (method === 'mfi'){
-        const stimuliAddFisher = arr.map((element) => ({fisherInformation: fisherInformation(theta,
-                {a: 1, b: element.difficulty, c: 0.5, d: 1}), ...element}));
-        stimuliAddFisher.sort((a,b) => b.fisherInformation - a.fisherInformation);
-        stimuliAddFisher.forEach((stimulus: Stimulus) => {delete stimulus['fisherInformation']});
-        return {
-            nextStimulus: stimuliAddFisher[0],
-            remainingStimuli: stimuliAddFisher.slice(1)
-        };
-    } else if (method === 'random') {
-        let index: number;
-        if (arr.length < 5){
-            index =  Math.floor(arr.length / 2);
-        } else {
-            index = Math.floor(arr.length / 2) + randomInteger(-2, 2);
-        }
-        const nextItem = arr[index];
-        arr.splice(index, 1);
-        return {
-            nextStimulus: nextItem,
-            remainingStimuli: arr
-        };
-    } else if (method === 'closest') {
-        //findClosest requires arr is sorted by difficulty
-        const index = findClosest(arr, theta + 0.481);
-        const nextItem = arr[index];
-        arr.splice(index, 1);
-        return {
-            nextStimulus: nextItem,
-            remainingStimuli: arr
-        };
+  /**
+   * return a random integer between min and max
+   * @param min - The minimum of the random number range (include)
+   * @param max - The maximum of the random number range (include)
+   * @returns {number} - random integer within the range
+   */
+  function randomInteger(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  /**
+   * find the item in a given array that has the difficulty closest to the target value
+   * @param arr Array<Stimulus> - an array of stimulus
+   * @param target number - ability estimate
+   * @returns {number} the index of arr
+   */
+  function findClosest(arr: Array<Stimulus>, target: number) {
+    const n = arr.length;
+    // Corner cases
+    if (target <= arr[0].difficulty) return 0;
+    if (target >= arr[n - 1].difficulty) return n - 1;
+    // Doing binary search
+    let i = 0,
+      j = n,
+      mid = 0;
+    while (i < j) {
+      mid = Math.ceil((i + j) / 2);
+      if (arr[mid].difficulty == target) return mid;
+      // If target is less than array
+      // element,then search in left
+      if (target < arr[mid].difficulty) {
+        // If target is greater than previous
+        // to mid, return closest of two
+        if (mid > 0 && target > arr[mid - 1].difficulty) return getClosest(arr, mid - 1, mid, target);
+        // Repeat for left half
+        j = mid;
+      }
+      // If target is greater than mid
+      else {
+        if (mid < n - 1 && target < arr[mid + 1].difficulty) return getClosest(arr, mid, mid + 1, target);
+        i = mid + 1; // update i
+      }
     }
+    // Only single element left after search
+    return mid;
 
-    /**
-     * return a random integer between min and max
-     * @param min - The minimum of the random number range (include)
-     * @param max - The maximum of the random number range (include)
-     * @returns {number} - random integer within the range
-     */
-    function randomInteger(min: number, max: number){
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+    function getClosest(arr: Array<Stimulus>, val1: number, val2: number, target: number) {
+      if (target - arr[val1].difficulty >= arr[val2].difficulty - target) return val2;
+      else return val1;
     }
+  }
 
-    /**
-     * find the item in a given array that has the difficulty closest to the target value
-     * @param arr Array<Stimulus> - an array of stimulus
-     * @param target number - ability estimate
-     * @returns {number} the index of arr
-     */
-    function findClosest(arr: Array<Stimulus>, target: number){
-        const n = arr.length;
-        // Corner cases
-        if (target <= arr[0].difficulty)
-            return 0;
-        if (target >= arr[n - 1].difficulty)
-            return n-1;
-        // Doing binary search
-        let i = 0, j = n, mid = 0;
-        while (i < j) {
-            mid = Math.ceil((i + j) / 2);
-            if (arr[mid].difficulty == target)
-                return mid;
-            // If target is less than array
-            // element,then search in left
-            if (target < arr[mid].difficulty) {
-                // If target is greater than previous
-                // to mid, return closest of two
-                if (mid > 0 && target > arr[mid - 1].difficulty)
-                    return getClosest(arr,mid-1, mid, target);
-                // Repeat for left half
-                j = mid;
-            }
-            // If target is greater than mid
-            else {
-                if (mid < n - 1 && target < arr[mid + 1].difficulty)
-                    return getClosest(arr,mid, mid + 1, target);
-                i = mid + 1; // update i
-            }
-        }
-        // Only single element left after search
-        return mid;
-
-        function getClosest(arr: Array<Stimulus>, val1:number, val2: number, target: number){
-            if (target - arr[val1].difficulty >= arr[val2].difficulty - target)
-                return val2;
-            else
-                return val1;
-        }
-    }
-
-    /**
-     * a 3PL Fisher information function
-     * @param theta - ability estimate
-     * @param zeta - item params
-     * @returns {number} - the expected value of the observed information
-     */
-    function fisherInformation(theta: number, zeta: Zeta){
-        const p = itemResponseFunction(theta, zeta)
-        const q = 1-p
-        return Math.pow(zeta.a, 2) * (q / p) * ((p - zeta.c) / Math.pow(1- zeta.c, 2))
-    }
-}
+  /**
+   * a 3PL Fisher information function
+   * @param theta - ability estimate
+   * @param zeta - item params
+   * @returns {number} - the expected value of the observed information
+   */
+  function fisherInformation(theta: number, zeta: Zeta) {
+    const p = itemResponseFunction(theta, zeta);
+    const q = 1 - p;
+    return Math.pow(zeta.a, 2) * (q / p) * ((p - zeta.c) / Math.pow(1 - zeta.c, 2));
+  }
+};
